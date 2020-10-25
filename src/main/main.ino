fa9aa99cb21a34f01e826e6cc641e68dbcfb0a4f
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-10-25 21:22:22 +0500

    added bme280

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,4 +1,7 @@
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 #define RX 8
 #define TX 9
@@ -6,11 +9,37 @@
 
 SoftwareSerial HC12(RX, TX);
 
+Adafruit_BME280 bme280;
+#define SEALEVELPRESSURE_HPA (1013.25)
 
 void setup() {
-  HC12.begin(9600);  
+  HC12.begin(9600);
+  if(bme280.begin(0x76))
+  {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while(true);
+  }  
 }
 
 void loop() {
-  HC12.write(0xAD);  
+  HC12.write(0xAD); 
+
+  Serial.print("Temperature = ");
+  Serial.print(bme280.readTemperature());
+  Serial.println("*C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme280.readPressure() / 100.0F);
+  Serial.println("hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme280.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println("m");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme280.readHumidity());
+  Serial.println("%");
+
+  Serial.println();
+  delay(1000);
 }
